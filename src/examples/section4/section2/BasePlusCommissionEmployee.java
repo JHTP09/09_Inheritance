ee4c64c1317735ba4d87a4ab27e39310feb0230a
Mohamed Elsayed <mody1first@yahoo.com>
2015-05-04 02:01:28 +0300

    Section 9.4.2: Creating and using a BasePlusCommissionEmployee class.

---
+++ src/examples/section4/section2/BasePlusCommissionEmployee.java
@@ -0,0 +1,113 @@
+package examples.section4.section2;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class BasePlusCommissionEmployee 
+{
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+    private double grossSales;      // gross weekly sales
+    private double commissionRate;  // commission percentage
+    private double baseSalary;      // base salary per week
+    
+    public BasePlusCommissionEmployee( String first, String last, String ssn,
+            double sales, double rate, double salary )
+    {
+        firstName = first;
+        lastName = last;
+        socialSecurityNumber = ssn;
+        setGrossSales( sales );
+        setCommissionRate( rate );
+        setBaseSalary( salary );
+    }
+    
+    public void setFirstName( String first )
+    {
+        firstName = first;  // should validate
+    }
+    
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    public void setLastName( String last )
+    {
+        lastName = last;    // should validate
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    public void setSocialSecurityNumber( String ssn )
+    {
+        socialSecurityNumber = ssn; //should validate
+    }
+    
+    public String getSocialSecurityNumber()
+    {
+        return socialSecurityNumber;
+    }
+    
+    public void setGrossSales( double sales )
+    {
+        if ( sales >= 0.0 )
+            grossSales = sales;
+        else
+            throw new IllegalArgumentException( 
+            "Gross sales must be >= 0" );
+    }
+    
+    public double getGrossSales()
+    {
+        return grossSales;
+    }
+    
+    public void setCommissionRate( double rate )
+    {
+        if ( rate > 0.0 && rate < 1.0 )
+            commissionRate = rate;
+        else
+            throw new IllegalArgumentException( 
+            "Commision rate must be > 0.0 and < 1.0" );
+    }
+    
+    public double getCommssionRate()
+    {
+        return commissionRate;
+    }
+    
+    public void setBaseSalary( double salary )
+    {
+        if ( salary >= 0 )
+            baseSalary = salary;
+        else
+            throw new IllegalArgumentException( "Base salary must be >= 0" );
+    }
+    
+    public double getBaseSalary()
+    {
+        return baseSalary;
+    }
+    
+    public double earnings()
+    {
+        return getBaseSalary() + ( getCommssionRate() * getGrossSales() );
+    }
+    
+    @Override
+    public String toString()
+    {
+        return String.format( "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f", 
+                "base-salaried commission employee", getFirstName(), getLastName(),
+                "social security number", getSocialSecurityNumber(),
+                "gross sales", getGrossSales(),
+                "commission rate", getCommssionRate(),
+                "base salary", getBaseSalary() );
+    }
+}
---
+++ src/examples/section4/section2/BasePlusCommissionEmployeeTest.java
@@ -0,0 +1,35 @@
+package examples.section4.section2;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class BasePlusCommissionEmployeeTest 
+{
+    public static void main( String[] args )
+    {
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee(
+                "Sami", "Yusuf", "222-22-2222", 10000, 0.05, 300 );
+        
+        System.out.println( "Employee information obtained by get methods:\n" );
+        System.out.printf( "%s %s\n", 
+                "First name is", employee.getFirstName() );
+        System.out.printf( "%s %s\n",
+                "Last name is", employee.getLastName() );
+        System.out.printf( "%s %s\n",
+                "Social security number is", 
+                employee.getSocialSecurityNumber() );
+        System.out.printf( "%s %.2f\n",
+                "Gross sales is", employee.getGrossSales() );
+        System.out.printf( "%s %.2f\n",
+                "Commssion rate is", employee.getCommssionRate() );
+        System.out.printf( "%s %.2f\n",
+                "Base salary is", employee.getBaseSalary() );
+        
+        employee.setBaseSalary( 1000 );
+        
+        System.out.println( 
+                "\nUpdated employee information obtained by toString: \n" );
+        System.out.println( employee );
+    }
+}
